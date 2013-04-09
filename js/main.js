21933c8b2543cde0a3d6de0c3d414d93047b16f3
Dan Peters <spartdan@live.co.uk>
2013-04-09 13:21:15 +0100

    Masonry update...

--- js/main.js
+++ js/main.js
@@ -2,6 +2,25 @@ $(function() {
 	"use strict";
 	/*jshint smarttabs:true */
 
-	
-	
+	var imageBoxSrc;
+
+	$('.imageBox').bind("click", function(event) {
+		imageBoxSrc = $(this).attr('href');
+		$('.lightbox-content').html('<img src="' + imageBoxSrc + '" />');
+		$('.lightbox').addClass('show');
+		return false;
+	});
+
+	$('.textBox').bind("click", function(event) {
+		$('.lightbox-content').html('Lightbox content, share, off site link etc.');
+		$('.lightbox').addClass('show');
+		return false;
+	});
+
+	$(document).keyup(function(e) {
+		if (e.keyCode == 27) {
+			$('.lightbox').removeClass('show');
+		}   // esc
+	});
+
 });
