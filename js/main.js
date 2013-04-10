0b717f7e09d5281c5f8466821e4d638b889521dd
Dan Peters <spartdan@live.co.uk>
2013-04-10 15:01:03 +0100

    Content loaded from JSON

--- js/main.js
+++ js/main.js
@@ -2,23 +2,56 @@ $(function() {
 	"use strict";
 	/*jshint smarttabs:true */
 
-	var imageBoxSrc,
+	// Base URL
+	var host = window.location.protocol + "//" + window.location.host;
+    var baseUrl = '/git/zerg/';
+    var baseUrl = host + baseUrl;
+
+	var box = $('.box'),
+		touched,
+		imageBoxSrc,
 		things;
 
-	/*$('.flip-container').bind('mouseover', flipper);
-	$('.flip-container').bind('mouseout', flipper);*/
-	$('.collectionTile').bind('click', flipper);
+	// Get the tiles
+	var source = baseUrl + 'source.json';
 
-	function flipper () {
-		if ( !$(this).hasClass('hover') ) {
+	// Load into the container
+	$.getJSON(source, function(data) {
+		$.each(data.tiles, function(i, item) {
+			//console.log(item.title);
+			if ( !item.media ) {
+				$('#container').append('<div id="box1" class="box col' + item.cols + ' collectionTile"><div class="collectionContainer"><div class="tile">' + item.title + '</div><div class="collectionInfo"><p>Other goodies can be added here</p></div></div></div>');
+			}
+			if ( item.media ) {
+				$('#container').append('<div id="box1" class="box col' + item.cols + ' img collectionTile"><div class="collectionContainer"><div class="tile"><img src="' + item.media + '" alt="" /></div><div class="collectionInfo"><p>Other goodies can be added here</p></div></div></div>');
+			}
+		});
+
+		$('#container').masonry({
+			itemSelector: '.box',
+			isFitWidth: true
+		}).imagesLoaded(function() {
+			$('#container').masonry('reload');
+		});
+	});
+
+	// Tile interaction
+	$(document).on('click', '.collectionTile', function(){
+		touched = $(this);
+		flipper(touched);
+	})
+
+	function flipper (touched) {
+		if ( !touched.hasClass('hover') ) {
 			$('.collectionTile').removeClass('hover');
-			$(this).addClass('hover');
+			touched.addClass('hover');
 		} else {
 			$('.collectionTile').removeClass('hover');
 		}
 		return false;
 	}
 
+	// Randomly tease the tile content
 	setInterval(function() {
 		$('.collectionTile').removeClass('tease');
 		things = $('.collectionTile');
@@ -28,6 +61,14 @@ $(function() {
 		}, 900);
 	}, 3900);
 
+	// Singular tiles
+	if(window.location.hash) {
+	  var hash = window.location.hash;
+	  box.addClass('hide');
+	  $(hash).removeClass('hide');
+	}
+
+	// Lightbox work - OLD - keeping incase
 	/*$('.imageBox').bind("click", function(event) {
 		imageBoxSrc = $(this).attr('href');
 		$('.lightbox-content > div').html('<img src="' + imageBoxSrc + '" />');
