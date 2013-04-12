e8a8c407d1f0516908164d7c28a5c2b448b416fb
Dan Peters <spartdan@live.co.uk>
2013-04-12 21:38:35 +0100

    tile content updates

--- js/main.js
+++ js/main.js
@@ -229,6 +229,7 @@ $(function() {
 			$('.lightbox-content > div').html(lightboxContent);
 			$('.lightbox').addClass('show');
 		});
+		$("html,body").animate({ scrollTop: 0 }, 300);
 		return false;
 	})
 
